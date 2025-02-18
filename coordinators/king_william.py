14822e1ec0daf6ee2e447e5d48201977bcfc0152
itxwi <w02.huang@surreyschools.ca>
2025-02-17 23:10:32 -0800

    probably final commit to king william test file

--- coordinators/king_william.py
+++ coordinators/king_william.py
@@ -7,12 +7,25 @@ import time
 
 dims = ['x','y','z']
 
+cansatOLED = Screen.OLED(fontsize=16)
+cansatOLED.clearImage()
+cansatOLED.drawFont("calibrating accelerometer",(10,0))
+cansatOLED.display()
 Gyro.calibrateAccel(delay=0)
+
+cansatOLED.clearImage()
+cansatOLED.drawFont("calibrating gyroscope",(10,0))
+cansatOLED.display()
 Gyro.calibrateGyro(delay=0)
-cansatOLED = Screen.OLED(fontsize=7)
+
+cansatOLED.clearImage()
+
+periods = 0
+hertz = .25
+gyroState = False #when true we are in sensor mode
 
 while True:
-    time.sleep(.5)
+    time.sleep(hertz)
     cansatOLED.clearImage()
     currentLog = Helper.getLog()
     gyroInfo = Gyro.getData()
@@ -23,14 +36,30 @@ while True:
         'gyro' : gyroInfo['gyro'],
         'sensor' : sensorInfo
         }
-    
 
 
     currentLog[time.time()] = newInfo
-    for i,dim in enumerate(dims):
-        cansatOLED.drawFont(f"{dim}: {gyroInfo['accel'][dim]}",(0,i*6))
-    
+
+    if periods>=7/hertz:
+        periods=periods%7/hertz
+        gyroState = not gyroState
+
+    #display stuff
+    if gyroState:
+        cansatOLED.drawFont("accel",(10,0))
+        for i,dim in enumerate(dims):
+            cansatOLED.drawFont(f"{dim}: {gyroInfo['accel'][dim]}",(10,(i+1)*16))
+
+        cansatOLED.drawFont("gyro",(74,0))
+        for i,dim in enumerate(dims):
+            cansatOLED.drawFont(f"{dim}: {gyroInfo['gyro'][dim]}",(74,(i+1)*16))
+    else:
+        cansatOLED.drawFont('Sensor',(10,0))
+        for reading in sensorInfo:
+            cansatOLED.drawFont(f"{reading}: {sensorInfo[reading]}",(10,(i+1)*16))
+
     cansatOLED.display()
 
     print(newInfo)
+    periods+=1
     Helper.writeLog(currentLog)
