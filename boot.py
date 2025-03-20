c58d0c975e36e69cdd259af413f48f89feb78fb0
itxwi <w02.huang@surreyschools.ca>
2025-03-19 21:27:09 -0700

    ill make sure to test before commiting nexttime

--- boot.py
+++ boot.py
@@ -10,6 +10,6 @@ COORDINATOR_PATH = os.path.join(BASE_DIR,"coordinators",f"{COORDINATOR}.py")
 time.sleep(WAIT)
 
 if os.path.isfile(COORDINATOR_PATH):
-    subprocess.run(['python3'],COORDINATOR_PATH)
+    subprocess.run(['python3',COORDINATOR_PATH])
 else:
     print(f"Error: Coordinator file {COORDINATOR_PATH} doesn't exist")
