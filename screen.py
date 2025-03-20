45f03a010c6a2953a09e79aae7c3c67a691d3c0b
itxwi <w02.huang@surreyschools.ca>
2025-03-19 23:53:34 -0700

    all this talk about a screen but i forgot to stage it lol must look crazy

---
+++ screen.py
@@ -0,0 +1,33 @@
+from PIL import Image, ImageDraw, ImageFont
+import Adafruit_SSD1306
+
+class OLED:
+    def __init__(self, font_size=16):
+        # initialize
+        self.disp = Adafruit_SSD1306.SSD1306_128_64(rst=None, i2c_address=0x3C)  # 3C standard address
+        self.disp.begin()
+        self.disp.clear()
+        self.width = self.disp.width
+        self.height = self.disp.height
+
+        self.image = Image.new('1', (self.width, self.height))  # 1 means binary colorscheme
+        self.draw = ImageDraw.Draw(self.image)
+
+        self.font = ImageFont.load_default()
+
+    def clear_image(self):
+        """clears image"""
+        self.disp.clear()
+
+    def display(self):
+        """displays current image"""
+        self.disp.image(self.image)
+        self.disp.display()
+
+    def draw_point(self, pos, bit=1):
+        """draws to image, bit is 1 or 0"""
+        self.draw.point(pos, fill=bit)
+
+    def draw_font(self, word, pos):
+        """draws text to image"""
+        self.draw.text(pos, word, font=self.font, fill=255)
