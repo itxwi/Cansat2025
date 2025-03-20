6ea042fd692936c3d26f0e716fed07ebc27d976c
itxwi <w02.huang@surreyschools.ca>
2025-03-19 21:25:22 -0700

    boot.py update

--- boot.py
+++ boot.py
@@ -1,8 +1,15 @@
-import time,os
+import time,os,subprocess
 
 # CONFIGS
 COORDINATOR = "five_pebbles"
 WAIT = 5
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+COORDINATOR_PATH = os.path.join(BASE_DIR,"coordinators",f"{COORDINATOR}.py")
 
 time.sleep(WAIT)
+
+if os.path.isfile(COORDINATOR_PATH):
+    subprocess.run(['python3'],COORDINATOR_PATH)
+else:
+    print(f"Error: Coordinator file {COORDINATOR_PATH} doesn't exist")
