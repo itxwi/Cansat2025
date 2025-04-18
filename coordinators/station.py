7b2d771a49efafbabbaf1588cb1413d0a5b68b0c
itxwi <w02.huang@surreyschools.ca>
2025-04-18 14:06:25 -0700

    station updates radio stuff

--- coordinators/station.py
+++ coordinators/station.py
@@ -1,3 +1,8 @@
+# coordinator requisit
+print("launched")
+import sys,os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import radio
 
 myRadio = radio.Radio(debug=True)
